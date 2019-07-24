bb9a820b3b259a1fd5b4098a57f50859960d6f36
Danilo Almeida <daniloa792@gmail.com>
2019-07-23 21:14:58 -0300

    Criar classe Pet

--- oopython.py
+++ oopython.py
@@ -1,4 +1,27 @@
+import datetime
 class Pessoa:
     def __init__(self,nome,idade):
         self.nome = nome 
         self.idade = idade
+    def getNome(self):
+        return self.nome
+    def getIdade(self):
+        return self.idade
+    def setNome(self, nome):
+        self.nome = nome
+    def setIdade(self , idade):
+        self.idade = idade
+
+class Pet(Pessoa):
+    def __init__(self, nome_pet, nasc_pet, nome, idade):
+        super().__init__(nome, idade)
+        self.nome_pet = nome_pet
+        self.nasc_pet = nasc_pet
+    def getNomePet(self):
+        return self.nome_pet
+    def getNascPet(self):
+        return self.nasc_pet
+    def setNomePet(self, nome_pet):
+        self.nome_pet = nome_pet
+    def setNascPet(self, nasc_pet):
+        self.nasc_pet = nasc_pet
